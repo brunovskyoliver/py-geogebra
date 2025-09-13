7579c6fabf07f1599b1a05b40e1b2318ceca96b0
brunovskyoliver <obrunovsky7@gmail.com>
2025-09-14 00:02:32 +0200

    basic layout

--- py_geogebra/widgets.py
+++ py_geogebra/widgets.py
@@ -5,7 +5,9 @@ class Widgets:
     def __init__(self):
         self._registry = []
 
-    def register(self, update_func: Callable[[], None]):
+    def register(
+        self, update_func: Callable[[], None]
+    ):  # https://docs.python.org/3/library/typing.html#annotating-callable-objects
         self._registry.append(update_func)
         update_func()
 
