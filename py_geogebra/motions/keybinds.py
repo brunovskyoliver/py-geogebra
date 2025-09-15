4152775dd24a3d48aab1d9ff8a7294120c95d43d
brunovskyoliver <obrunovsky7@gmail.com>
2025-09-15 11:33:00 +0200

    deleting points

---
+++ py_geogebra/motions/keybinds.py
@@ -0,0 +1,11 @@
+from .. import state
+from ..tools.utils import delete_object
+
+
+def keybinds(root, canvas, objects, axex):
+    def keypressed(e):
+        if e.keysym == "BackSpace":
+            if state.selected_point != None and state.selected_tool == "arrow":
+                delete_object(canvas, objects, state.selected_point, state)
+
+    canvas.bind("<KeyPress>", keypressed)
