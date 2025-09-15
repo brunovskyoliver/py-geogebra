a4ce0c034326405ca61c95d6a63f36386907352e
brunovskyoliver <obrunovsky7@gmail.com>
2025-09-15 14:23:19 +0200

    som genius

--- py_geogebra/tools/utils.py
+++ py_geogebra/tools/utils.py
@@ -13,6 +13,13 @@ def number_to_ascii(n: int):
     return s
 
 
+def ascii_to_number(s: str):
+    n = 0
+    for ch in s:
+        n = n * 26 + (ord(ch) - 65 + 1)
+    return n - 1
+
+
 def center(canvas: tk.Canvas, objects):
     width = canvas.winfo_width()
     height = canvas.winfo_height()
@@ -27,9 +34,20 @@ def set_cursor(canvas: tk.Canvas, cursor: str):
     canvas.focus_set()
 
 
+def reconfigure_label_order(label: str, state):
+    state.label_list.remove(label)
+    state.label_counter_bck = state.label_counter
+    state.label_counter = ascii_to_number(label)
+
+
 def delete_object(canvas, objects, object_to_delete, state):
+    from ..ui.point import Point
+
     objects.unregister(object_to_delete)
     canvas.delete(object_to_delete.tag)
     if hasattr(object_to_delete, "highlight_tag"):
         canvas.delete(object_to_delete.highlight_tag)
+    if isinstance(object_to_delete, Point):
         state.selected_point = None
+        label = object_to_delete.label
+        reconfigure_label_order(label, state)
