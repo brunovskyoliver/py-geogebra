75ae96f863ffe41f0c2d3191fec248241e9dc697
brunovskyoliver <obrunovsky7@gmail.com>
2025-09-16 07:24:18 +0200

    couple touches

--- py_geogebra/ui/segment_with_lenght.py
+++ py_geogebra/ui/segment_with_lenght.py
@@ -30,7 +30,7 @@ class Segment_with_length:
 
         self.cx = 0
         self.cy = 0
-        
+
         self.length = length
         self.angle = angle
 
@@ -44,18 +44,20 @@ class Segment_with_length:
         self.canvas.delete(self.tag)
 
         visual_scale = min(max(1, self.scale**0.5), 1.9)
-        if (state.drag_target is self.point_2):
-            self.angle = math.atan2(self.point_2.pos_y - self.point_1.pos_y, self.point_2.pos_x - self.point_1.pos_x)
+        if state.drag_target is self.point_2:
+            self.angle = math.atan2(
+                self.point_2.pos_y - self.point_1.pos_y,
+                self.point_2.pos_x - self.point_1.pos_x,
+            )
 
         x1, y1 = self.point_1.pos_x, self.point_1.pos_y
         self.point_2.pos_x = x1 + self.length * math.cos(self.angle) / (self.unit_size)
         self.point_2.pos_y = y1 + self.length * math.sin(self.angle) / (self.unit_size)
         x2, y2 = self.point_2.pos_x, self.point_2.pos_y
-        self.point_2.update()  
-        
+        self.point_2.update()
 
-        x1, y1 = world_to_screen(self.canvas, self.objects, x1, y1)
-        x2, y2 = world_to_screen(self.canvas, self.objects, x2, y2)
+        x1, y1 = world_to_screen(self.objects, x1, y1)
+        x2, y2 = world_to_screen(self.objects, x2, y2)
 
         self.canvas.create_line(
             x1,
@@ -66,5 +68,3 @@ class Segment_with_length:
             width=2 * visual_scale,
             tags=self.tag,
         )
-
-
