309eaf483cafd03a22b831eca80a61edb7b6eefb
brunovskyoliver <obrunovsky7@gmail.com>
2025-09-30 07:23:27 +0200

    geeenius

--- py_geogebra/ui/dialogs.py
+++ py_geogebra/ui/dialogs.py
@@ -1,5 +1,7 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, filedialog
+import sys, json, os
+from .. import globals
 
 
 def ask_for_update(widgets):
@@ -20,3 +22,29 @@ def ran_from_python(filepath):
     messagebox.showinfo(
         title=_("Nespustil si binary"), message=_(f"Binary nájdeš v {filepath}")
     )
+
+
+def open_from_file(root):
+    if getattr(
+        sys, "frozen", False
+    ):  # https://pyinstaller.org/en/stable/runtime-information.html
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+
+    file = filedialog.askopenfile(defaultextension="json", initialdir=base_path)
+    data = json.load(file)
+    globals.objects.load_from_dict(root, data)
+
+
+def save_file(root):
+    if getattr(
+        sys, "frozen", False
+    ):  # https://pyinstaller.org/en/stable/runtime-information.html
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+
+    file = filedialog.asksaveasfilename(defaultextension="json", initialdir=base_path)
+
+    globals.objects.to_json(file)
