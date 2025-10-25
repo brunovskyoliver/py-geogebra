cb6fbfee6e9cdb4b24e44a8016df72a1ea6912e4
brunovskyoliver <obrunovsky7@gmail.com>
2025-10-25 17:26:07 +0200

    fix .env

--- py_geogebra/tools/auth_config.py
+++ py_geogebra/tools/auth_config.py
@@ -1,7 +1,14 @@
-from dotenv import load_dotenv
 import os
-load_dotenv()
+from dotenv import load_dotenv
+import sys
 
+if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+    env_path = os.path.join(sys._MEIPASS, '.env')
+else:
+    env_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+
+
+load_dotenv(env_path)
 TURSO_URL = os.getenv("TURSO_URL")
 TURSO_AUTH_TOKEN = os.getenv("TURSO_AUTH_TOKEN")
 AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN")
